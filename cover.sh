171d40459cc92d4a3bc4b7f78b0dd0a159aebeb8
John McCabe <john@johnmccabe.net>
2018-02-01 16:12:41 +0000

    Update paths in cover.sh
    This commit updates the paths in cover.sh which had gone out of sync
    with the current repos. Note that it currently fails with errors in
    go-evaluator.

--- cover.sh
+++ cover.sh
@@ -8,22 +8,22 @@ github.com/puppetlabs/go-parser/parser\
 /usr/local/go/bin/go test -coverprofile c3.out -coverpkg \
 github.com/puppetlabs/go-pspec/pspec,\
 github.com/puppetlabs/go-evaluator/eval,\
-github.com/puppetlabs/go-evaluator/evaluator,\
+github.com/puppetlabs/go-evaluator/impl,\
 github.com/puppetlabs/go-evaluator/pcore,\
 github.com/puppetlabs/go-evaluator/types,\
 github.com/puppetlabs/go-parser/parser,\
 github.com/puppetlabs/go-parser/validator\
- github.com/puppetlabs/go-pspec/eval_test
+ github.com/puppetlabs/go-evaluator/eval_test
 
 /usr/local/go/bin/go test -coverprofile c4.out -coverpkg \
 github.com/puppetlabs/go-pspec/pspec,\
 github.com/puppetlabs/go-evaluator/eval,\
-github.com/puppetlabs/go-evaluator/evaluator,\
+github.com/puppetlabs/go-evaluator/impl,\
 github.com/puppetlabs/go-evaluator/pcore,\
 github.com/puppetlabs/go-evaluator/types,\
 github.com/puppetlabs/go-parser/parser,\
 github.com/puppetlabs/go-parser/validator\
- github.com/puppetlabs/go-pspec/parser_test
+ github.com/puppetlabs/go-parser/parser_test
 
 tail -n+2 c2.out >> c1.out
 tail -n+2 c3.out >> c1.out
